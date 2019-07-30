dd9f7358dfbfe0f713131cd5b465780b57682da9
hari2951 <hari4537@gmail.com>
2019-07-30 17:12:54 +0100

    Newfiles

---
+++ src/components/BookList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookList',
+  data() {
+    return {
+      msg: 'All Books',
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
