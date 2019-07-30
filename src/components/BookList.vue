db844e8641d7cf6bbba43f7e9c31a4ebf6fe5013
hari2951 <hari4537@gmail.com>
2019-07-30 17:29:45 +0100

    Module3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ title }}</h1>
+    <ul>
+        <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+    </ul>
   </div>
 </template>
 
@@ -10,6 +13,11 @@ export default {
   data() {
     return {
       title: 'All Books',
+      books:[
+        {title:'Self-Reliance', author:'Ralph'},
+        {title:'Self-Reliance2', author:'Hari'},
+        {title:'Self-Reliance3', author:'Linta'},
+      ]
     };
   },
 };
@@ -19,6 +27,15 @@ export default {
 <style scoped>
 h1, h2 {
   font-weight: normal;
+}
+
+ul{
+  list-style-type:normal;
+  padding:0;
+}
+li{
+display:block;
+margin:0 10px;
 }
 
 </style>
