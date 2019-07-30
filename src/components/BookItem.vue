f0e8cfa74853f2e4f558169b2f04a88eb21c3fb2
hari2951 <hari4537@gmail.com>
2019-07-30 17:36:50 +0100

    Module4

---
+++ src/components/BookItem.vue
@@ -0,0 +1,26 @@
+<template>
+   <li>{{book.title}}:{{book.author}}</li>
+</template>
+
+<script>
+
+  export default {
+    name: 'BookItems',
+    props: [book]
+  
+    
+  }
+
+
+</script>
+
+
+<style scoped>
+
+
+li{
+display:block;
+margin:0 10px;
+}
+
+</style>
